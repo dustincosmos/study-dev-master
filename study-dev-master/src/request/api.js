b9a745ab0e35586f007cbf6c0b8d6781004aafaf
ikscut <1582068144@qq.com>
2022-06-16 22:58:05 +0800

    test

--- study-dev-master/src/request/api.js
+++ study-dev-master/src/request/api.js
@@ -1,2 +1,3 @@
 import Network from "./network";
 export const register = (data) => Network.post("/user/register", data);
+// export const queryPoint = (data) => Network.post(`/kp/get/${id}`, data);
